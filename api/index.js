87a76cd920ac669b46a16ce1f036cab3ea96daae
Vibhav-Jaiswal <vibhavjaiswal9793@gmail.com>
2024-04-05 20:13:32 +0530

    create a test api route

--- api/index.js
+++ api/index.js
@@ -1,9 +1,12 @@
 import express from 'express'
 import dotenv from 'dotenv'
+import userRouter from './routes/user.route.js'
 
 dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 9999;
+
+app.use('/api', userRouter)
 
 app.listen(PORT,()=>{
     console.log('Server is running...');
